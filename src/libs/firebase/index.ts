535947b27ecf25ee8f926e8805e9737a3ff74131
nabeliwo <nabeliwo@gmail.com>
2023-06-13 00:52:34 +0900

    :wrench: 環境変数にする必要のないものはやめる

--- src/libs/firebase/index.ts
+++ src/libs/firebase/index.ts
@@ -4,9 +4,9 @@ import { getDatabase } from 'firebase/database'
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  authDomain: 'mf-expense-viewer.firebaseapp.com',
+  projectId: 'mf-expense-viewer',
+  storageBucket: 'mf-expense-viewer.appspot.com',
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
